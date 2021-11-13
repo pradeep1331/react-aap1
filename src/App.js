2c90c59a226811aad8dbbb785e7f4d7921a5979f
Pradeep S <spradee1331@gmail.com>
2021-11-13 10:30:31 +0530

    changes committed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Button from '@mui/material/Button';
 import { Movie } from "./movieslist";
 import Input from '@mui/material/Input';
+import { Switch, Route, Link ,useParams} from "react-router-dom";
 
 //const marvel = ;
 
@@ -16,6 +17,7 @@ function App() {
         "https://www.denofgeek.com/wp-content/uploads/2019/04/ant-man-1-main.jpg?resize=768%2C432",
       summary:
         "Ant-Man is a 2015 American superhero film based on the Marvel Comics characters of the same name: Scott Lang and Hank Pym.",
+        trailer:"https://www.youtube.com/embed/pWdKf3MneyI"
     },
     {
       movie: "Ironman",
@@ -24,14 +26,16 @@ function App() {
         "https://www.cnet.com/a/img/bZaqv6tPvT44-cop4ZL2gG3j5wE=/940x0/2020/01/17/7da55a03-ac5b-4ec1-b59b-6b3c2414e68b/egdt5idw4aittju.jpg",
       summary:
         "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee",
+        trailer:"https://www.youtube.com/embed/8ugaeA-nMTc"
     },
     {
-      movie: "Black Panther",
+      movie: "BlackPanther",
       rating: 8,
       poster:
         "https://image.cnbcfm.com/api/v1/image/102129219-544fe3109623b.jpg?v=1497046818&w=1600&h=900",
       summary:
         "Black Panther is a 2018 American superhero film based on the Marvel Comics character of the same name.",
+        trailer:"https://www.youtube.com/embed/xjDjIWPwcPU"
     },
   ]);
 
@@ -51,8 +55,34 @@ function App() {
   };
   return (
     // <Addcolor/>
+
     <div className="App">
-      <h2>Add Movies Here</h2>
+      <div className="app-list">
+      <ul>
+      <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <a href="/Movies">Movies Page</a>
+        </li>
+        <li>
+          {/* Change the url bar but dont refresh */}
+          <Link to="/Color">Colors Page</Link>
+        </li>
+        <li>
+          {/* Change the url bar but dont refresh */}
+          <Link to="/AddMovie">Add Movie</Link>
+        </li>
+      
+        </ul>
+        </div>
+
+        <Switch>
+          <Route exact path="/">
+           <h1>Welcome to Home Page</h1>
+          </Route>
+          <Route path="/AddMovie">
+          <h2>Add Movies Here</h2>
       <label>Moviename &nbsp;</label>
       <Input
         name="moviename"
@@ -96,11 +126,14 @@ function App() {
       <Button id="" variant="contained" onClick={addMovie}>
         AddMovie
       </Button>
-      <br></br><br></br>
+          </Route>
+          <Route path="/Movies">        
 
       <section className="movie-list">
-        {movies.map(({ movie, rating, poster, summary }) => (
+        {movies.map(({ movie, rating, poster, summary },index) => (
           <Movie
+        
+           index={index}
             movie={movie}
             rating={rating}
             poster={poster}
@@ -108,6 +141,23 @@ function App() {
           />
         ))}
       </section>
+      </Route>
+
+          <Route path="/movies/:id">
+          <MovieDetails movies={movies}/>
+          </Route>
+
+          <Route path="/Color">
+          <Addcolor/>
+          </Route>
+          <Route exact path="**"> 
+           <NotFound />
+
+          </Route>
+          
+        </Switch>
+        
+     
     </div>
 
     // <Counter/>
@@ -128,14 +178,14 @@ function App() {
 **/
 /*function Movie({ movie, rating, poster, summary }) {
   return (
-    <div class="movie-container">
+    <div className="movie-container">
      
       <img className="movieposter" src={poster} heigth="200" width="200" alt="Movie Poster" />
       <div className ="movie-specs">
       <h2 className="movie">{movie}</h2>
       <h2>⭐ Rating:{rating}</h2>
       </div>
-      <h2 class="summary">{summary}</h2>
+      <h2 className="summary">{summary}</h2>
       <div>
       <Counter />
     </div>
@@ -144,7 +194,7 @@ function App() {
 }
 */
 
-/*function Addcolor(){
+function Addcolor(){
   const [color,setColor] = useState("");
   const styles = {backgroundColor:color};
  const [colors,setColors] = useState(["pink","orange","crimson"]);
@@ -160,11 +210,34 @@ return(
 
 }
 
-*/
-
-/*function ColorBox({clr}){
+function ColorBox({clr}){
   const styles= {backgroundColor:clr,height:"50px",width:"50px"};
   return <div style ={styles}></div> ;
 }
-*/
+
+function NotFound(){
+   const styles =  {width:"100%"};
+  return(
+    <img style={styles} src="https://img-16.ccm2.net/_SqzzXVDSG50FWb_UBrCl3XwV78=/440x/1685e17045e747a899925aa16189c7c6/ccm-encyclopedia/99776312_s.jpg" alt="Page Not Found"></img>
+  );
+}
+
+function MovieDetails({movies}){
+  const {id} = useParams();
+  const movie = movies[id];
+  return ( 
+      <div>
+    <iframe width="669" height="361" src={movie.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div className="movie-container">
+
+      <div className ="movie-specs"> 
+      
+      <h2>⭐ Rating:{movie.rating}</h2>
+      <h2 class="summary">{movie.summary}</h2>
+      </div>
+      </div>
+      </div>
+    );
+}
+
 export default App;
--- src/movieslist.js
+++ src/movieslist.js
@@ -1,23 +1,61 @@
 import { useState } from "react";
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Badge from '@mui/material/Badge';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';  
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import { useHistory } from "react-router-dom";
+import EditIcon from '@mui/icons-material/Edit';
+import {Link,Redirect,Route,Switch} from "react-router-dom";
 export {Movie};
 
-function Movie({ movie, rating, poster, summary }) {
+
+function Movie({ movie, rating, poster, summary,index }) {
  // const [show,setShow] = useState(true);
  // const styles = {};
+  
+    const [show,setShow] = useState(true);
+    const history = useHistory();
+    const styles = {display: show ? " ":"none" }
     return (
-      <div class="movie-container">
+      <Card class="movie-container">
        
         <img className="movieposter" src={poster} heigth="200" width="200" alt="Movie Poster" />
         <div className ="movie-specs">
-        <h2 className="movie">{movie}</h2>
+     
+        <h2 className="movie">{movie}<IconButton color="secondary" onClick={()=>setShow(!show)} style={{marginBottom:"10px"}}>
+          {show ?<ExpandLessIcon/>:<ExpandMoreIcon/>}
+        
+        </IconButton> 
+        <IconButton color="primary" onClick={()=>{history.push(`/movies/${index}`)}}>
+          <VideoLibraryIcon/>
+          </IconButton> 
+          <IconButton >
+          <DeleteIcon/>
+          </IconButton> 
+          <IconButton >
+          <EditIcon />
+          </IconButton> 
+        </h2>
+      
         <h2>⭐ Rating:{rating}</h2>
+    
         </div>
-        <h2 class="summary">{summary}</h2>
+    
+        {/* <Button onClick={()=>setShow(!show)} style={{marginBottom:"10px"}} variant="outlined">{show ?"Hide":"show"} Description</Button>*/}
+        <CardContent>
+        <h2 style={styles} class="summary">{summary}</h2>
+        </CardContent>
         <div>
         <Counter />
+      
       </div>
-      </div>
+      </Card>
     );
   }
 
@@ -27,9 +65,17 @@ function Movie({ movie, rating, poster, summary }) {
     const [like,setlike] = useState(0);
     return(
       <div className="btn">
-        <Button variant="outlined"  onClick={()=>setdislike(dislike+1)}>👎 dislike{dislike}</Button>
-        <Button  variant="outlined" onClick={()=>setlike(like+1)}>👍 like{like}</Button>
-      </div>
+        <IconButton aria-label="likes"  color="primary" onClick={()=>setlike(like+1)}>
+        <Badge badgeContent={like} color="primary">
+        👍 
+    </Badge>
+      </IconButton>
+      <IconButton aria-label="dislikes"  color="secondary" onClick={()=>setdislike(like+1)}>
+      <Badge badgeContent={dislike} color="error">
+      👎  
+    </Badge>
+      </IconButton>
+       </div>
     );
   }
 
